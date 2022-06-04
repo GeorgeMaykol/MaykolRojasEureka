0f5144d3104709ada9ad76572da50044e1b9693d
Maykol Rojas <maykolroj@gmail.com>
2022-06-04 07:40:29 -0500

    Terminado el Eureka Server con 3 microservicios

---
+++ src/test/java/com/idat/MaykolRojasEurekaServer/MaykolRojasEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.MaykolRojasEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaykolRojasEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
